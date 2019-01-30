1c281f897a523c031e58f8fd1f588f830814b55a
Fredrik Iselius <fredrik.iselius@gmail.com>
2019-01-30 23:02:54 +0100

    Added htmlize package to emacs

--- emacs/init.el
+++ emacs/init.el
@@ -24,6 +24,7 @@
     neotree
     atom-one-dark-theme
     org-bullets
+    htmlize
     py-autopep8))
 
 (mapc #'(lambda (package)
@@ -101,7 +102,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files (quote ("~/2.org")))
- '(package-selected-packages (quote (matlab-mode material-theme better-defaults))))
+ '(package-selected-packages
+   (quote
+    (htmlize matlab-mode material-theme better-defaults))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
