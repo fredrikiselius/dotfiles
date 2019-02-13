0d97728125884b78723f3eff5fc0e39c2ec78eee
freis685 <freis685@tlhw-3-3.ad.liu.se>
2019-02-13 13:51:57 +0100

    Added alias for NLP course virtual environment

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -18,5 +18,8 @@ alias la='ls -lAh --color=auto'
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
+# Add check for school system
+alias nlpenv='source /courses/TDDE09/labs/environment/bin/activate'
+
 # Use emacs in terminal
 #alias emacs='emacs -nw'
